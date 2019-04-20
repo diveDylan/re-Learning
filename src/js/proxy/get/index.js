5926232eb23d4f5db013a0f2b62a37da9c415182
曾祥达 <zengxiangda@zhongan.io>
2019-04-20 17:26:13 +0800

    :speech_balloon: finish proxy get md

--- src/js/proxy/get/index.js
+++ src/js/proxy/get/index.js
@@ -4,11 +4,26 @@ const Dylan = {
   [Symbol('like')]: 'dive'
 }
 
+Object.defineProperty(Dylan, 'gender', {
+  configurable: false,
+  enumerable: false,
+  writable: false,
+  value: 'Male'
+})
+
 let proxyDy = new Proxy(Dylan, {
   get: function(target, prop, recevier) {
     console.log(this, 'getting')
     console.log(target, prop, recevier)
+    if(prop === 'gender') return 'female'
+    return Reflect.get(target, prop, recevier)
+  },
+  set: function(target, prop, val) {
+    console.log('setting:', target, prop, val)
+    return Reflect.set(target, prop, val)
   }
 })
 
-console.log(proxyDy.name)
+proxyDy.gender
+console.log(proxyDy.gender)
+console.log(proxyDy.name, Dylan.gender, proxyDy[Symbol('like')])
