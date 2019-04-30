d4364beefc2fbb3cf03b4b3e48ef24b71c0a6fa7
曾祥达 <zengxiangda@zhongan.io>
2019-04-30 22:58:36 +0800

    String.prototype.trim

---
+++ src/js/string/trim/index.js
@@ -0,0 +1,10 @@
+// 业务中trim
+//  username 用户名首尾不能有空格
+const userName = "      dylan   " + '   '
+console.log(':'+ userName.trim() +':')
+
+// 公司类名字
+const company = "  zhongan.io "
+console.log(company.trim(), company)
+
+
