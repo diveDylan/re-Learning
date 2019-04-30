0e7335f252955d69828b13fcbe5472d599c864e6
曾祥达 <zengxiangda@zhongan.io>
2019-04-30 23:20:50 +0800

    :smile: update repeat

--- src/js/string/repeat/index.js
+++ src/js/string/repeat/index.js
@@ -8,3 +8,22 @@ str.repeat(1) // I'm dylan
 str.repeat(1.8) // 1、Math.floor(1.8) 2、output: I'm dylan 
 str.repeat(2) // I'm dylan I'm dylan 
 
+/**
+ * step 1: f --> f.toString()
+ * step 2: String.prototpye.repeat.call(f.toString(), count)
+ * 
+ */
+
+function repeat(f) {
+  const a = String.prototype.repeat.call(f, 1)
+  console.log(a)
+  return a
+}
+const arr = [1,2,3]
+repeat(arr) // 1,2,3
+arr.toString = () => 'add'
+repeat(arr) // add
+const obj = {
+  toString: () => 'dylan'
+}
+repeat(obj)
